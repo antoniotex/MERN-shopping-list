2be4418b6cd0e9b7323446085d09493c62b200a9
unknown <antoniotx.dev@gmail.com>
2019-02-01 11:31:05 -0200

    Adiciona modulos para usar o redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import { Provider } from 'react-redux'
+import store from './store'
 
 class App extends Component {
   render() {
